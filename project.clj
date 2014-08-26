bdac7437b5e81adede708b641e8134a624cf129d
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2014-08-27 00:39:34 +0100

    basic liberator

--- project.clj
+++ project.clj
@@ -1,7 +1,14 @@
 (defproject owlview "0.1.0-SNAPSHOT"
   :description "OWL viewer"
   :url "https://github.com/stain/owlview"
-  :license {:name "Apache License 2.0"
-            :url "https://www.apache.org/licenses/LICENSE-2.0"}
+  :scm {:connection "scm:git:https://github.com/stain/owlview.git"
+        :url "https://github.com/stain/owlview"}
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments "same as Clojure"}
   :plugins [[lein-ring "0.8.11"]]
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :ring {:handler owlview.core/handler}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [liberator "0.12.0"]
+  ])
--- src/owlview/core.clj
+++ src/owlview/core.clj
@@ -1,6 +1,18 @@
-(ns owlview.core)
+(ns owlview.core
+  (:require [liberator.core :refer [resource defresource]]
+            [ring.middleware.params :refer [wrap-params]]
+            [ring.adapter.jetty :refer [run-jetty]]
+            [compojure.core :refer [defroutes ANY GET POST PUT DELETE]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(defroutes app
+  (ANY "/" [] (resource :available-media-types ["text/html" "application/xhtml+xml"]
+                        :handle-ok "<html xmlns='http://www.w3.org/1999/xhtml'><body>Welcome</body></html>"))
+  (ANY "/foo" [] (resource :available-media-types ["text/html"]
+                           :handle-ok (fn [ctx]
+                                        (format "<html>It's %d milliseconds since the beginning of the epoch."
+                                                (System/currentTimeMillis))))))
+
+(def handler
+  (-> app
+      (wrap-params)))
