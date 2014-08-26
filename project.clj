fbde6da9809063829cdc780ac8f2be8dd0e5ed5e
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2014-08-27 00:28:07 +0100

    lein ring

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject owlview "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "OWL viewer"
+  :url "https://github.com/stain/owlview"
+  :license {:name "Apache License 2.0"
+            :url "https://www.apache.org/licenses/LICENSE-2.0"}
+  :plugins [[lein-ring "0.8.11"]]
   :dependencies [[org.clojure/clojure "1.6.0"]])
