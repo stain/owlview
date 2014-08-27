619ce1361ade5348a0771e76689bb96c4863d285
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2014-08-27 01:36:08 +0100

    some bootstrap

--- src/owlview/core.clj
+++ src/owlview/core.clj
@@ -1,17 +1,38 @@
 (ns owlview.core
   (:require [liberator.core :refer [resource defresource]]
             [ring.middleware.params :refer [wrap-params]]
+            [hiccup.core :refer [html]]
+            [hiccup.page :refer [html4 html5 xhtml include-css include-js]]
             [ring.adapter.jetty :refer [run-jetty]]
             [compojure.core :refer [defroutes ANY GET POST PUT DELETE]]))
 
+(defn xhtml? [ctx]
+;  (= "application/xhtml+xml";
+;     (print
+;    (get-in ctx [:representation :media-type]))
+    false)
 
 (defroutes app
   (ANY "/" [] (resource :available-media-types ["text/html" "application/xhtml+xml"]
-                        :handle-ok "<html xmlns='http://www.w3.org/1999/xhtml'><body>Welcome</body></html>"))
-  (ANY "/foo" [] (resource :available-media-types ["text/html"]
-                           :handle-ok (fn [ctx]
-                                        (format "<html>It's %d milliseconds since the beginning of the epoch."
-                                                (System/currentTimeMillis))))))
+                        :handle-ok (fn [ctx] (html5 {:xml? (xhtml? ctx)}
+                            [:head
+                              [:title "owlview"]
+                              (include-css "//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.css"
+                                "//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.css"
+                                )]
+                            [:body
+                             [:div {:class "container"}
+                              [:h1 "Hiccup"]
+                              [:div {:class "container theme-showcase" :role "main"}
+                                  "Hello there!"
+                                  [:p [:a {:href "#"
+                                       :class "btn btn-primary btn-lg"
+                                       :role "main"} "Do it"]]
+                                ]
+                              [:p "Welcome to hiccup"]]
+                              (include-js "https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.js"
+                                          "//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.js")
+                          ])))))
 
 (def handler
   (-> app
