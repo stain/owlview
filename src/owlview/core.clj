52337f64160f68af7d377b807926445b23e92f50
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2014-09-02 16:03:09 +0100

    error handling page

--- src/owlview/core.clj
+++ src/owlview/core.clj
@@ -2,8 +2,7 @@
   (:require [liberator.core :refer [resource defresource]]
             [liberator.dev :refer [wrap-trace]]
             [ring.middleware.params :refer [wrap-params]]
-            [hiccup.core :refer [html]]
-            [hiccup.page :refer [html4 html5 xhtml include-css include-js]]
+            [hiccup.page :refer [html5 include-css include-js]]
             [ring.adapter.jetty :refer [run-jetty]]
             [owlapi.core :refer [with-owl load-ontology loaded-ontologies classes
                                 owl-manager with-owl-manager
@@ -26,51 +25,55 @@
     (with-owl-manager owl-manager
       (load-ontology uri))))
 
+(defn html [ctx title & body]
+              (html5 {:xml? (xhtml? ctx)}
+                  [:head
+                    [:title title]
+                    (include-css "//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.css"
+                      "//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.css"
+                      )]
+                  [:body
+                   [:div {:class "container"}
+                    [:h1 title]
+                    body
+                    [:address {:class "footer"}
+                      [:a {:href "https://github.com/stain/owlview"} "owlview"]
+                      " by "
+                      [:a {:href "http://orcid.org/0000-0001-9842-9718"}
+                      "Stian Soiland-Reyes"]]
+                    (include-js "https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.js"
+                                "//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.js")
+                  ]]))
+
 (defroutes app
   (ANY "/" [] (resource
     :available-media-types ["text/html" "application/xhtml+xml"]
-
-                        :handle-ok (fn [ctx] (html5 {:xml? (xhtml? ctx)}
-                            [:head
-                              [:title "owlview"]
-                              (include-css "//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.css"
-                                "//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.css"
-                                )]
-                            [:body
-                             [:div {:class "container"}
-                              [:h1 "owlview"]
+                        :handle-ok (fn [ctx] (html ctx "owlview"
                               [:div {:class "jumbotron"}
                                   "Visualize an OWL/RDFS ontology:"
-                                  [:form {:role "form" :method "POST" :action "ont/"}
+                                  [:form {:role "form" :method "POST" :action "ont"}
                                      [:p [:input {:name "url" :type "url" :class "form-control" :placeholder "http://purl.org/pav/" :autofocus :autofocus}]]
                                      "or:"
                                      [:p [:input {:name "file" :type "file" :class "form-control"
                                                     :accept "application/rdf+xml,text/turtle,application/owl+xml,.owl,.rdf,.ttl,.owx"}]]
                                      [:p [:input {:type "submit" :class "btn btn-primary btn-lg" :value "Visualize"}]]
                                   ]
-                              ]]
-                              [:address {:class "footer"}
-                                [:a {:href "https://github.com/stain/owlview"} "owlview"]
-                                " by "
-                                [:a {:href "http://orcid.org/0000-0001-9842-9718"}
-                                "Stian Soiland-Reyes"]]
-                              (include-js "https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.js"
-                                          "//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.js")
-                          ]))))
-  (ANY "/ont/" [] (resource
-      :available-media-types ["text/html"]
+                              ]
+                          ))))
+  (ANY "/ont" [] (resource
+      :available-media-types ["text/html" "application/xhtml+xml"]
       :allowed-methods [:post :get]
-      :handle-ok (fn [ctx] (html5 [:body "Known:" (keys @known-ontologies) "OK?"]))
+      :handle-ok (fn [ctx] (html ctx "owlview: Known ontologies" ["Known:" (keys @known-ontologies) "OK?"]))
       :post! (fn [ctx] (str "OK. " ctx))
       :post-redirect? (fn [ctx] {:location (format "/ont/%s" "http://purl.org/pav/")})
     ))
   (ANY "/ont/*" [& {url :*}] (resource
     :available-media-types ["text/html" "application/xhtml+xml"]
-    :handle-exception (fn [{e :exception}] (throw e))
+    :handle-exception (fn [{err :exception :as ctx}] (html ctx (str "Failed to load ontology " url) (or (.getMessage err) (str err))))
     :handle-ok (fn [ctx]
         (with-owl
           (let [ontology (get-ontology url)]
-          (html5 {:xml? true} [:body [:div "OK then, " url]])
+            (html ctx (str "Ontology " url) [:div "OK that is, " url])
         )))))
 )
 
